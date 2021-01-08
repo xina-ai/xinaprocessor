1b4486f06e6d49a392bd8a587ffdfe0e1032fd99
TRoboto <m4bh@hotmail.com>
2021-01-08 21:04:54 +0200

    add setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='xinaprocessor',
-    version='1.0.0',
-    install_requires=[
-        'pytest'
-    ],
-    packages=find_packages()
+    version='0.1.0',
+    install_requires=required,
+    author="Xina AI",
+    description="Arabic processing library",
+    packages=find_packages(include=['xinaprocessor'])
 )
