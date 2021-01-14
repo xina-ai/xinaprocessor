9e2294766ee858d21c670b4e7ecb257269d3eadd
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-01-14 09:29:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='xinaprocessor',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=required,
     tests_require=['pytest'],
     author="Xina AI",
