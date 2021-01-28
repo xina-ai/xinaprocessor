5bd457f25becb2d3dfb2a60646c01ecf97c60a8a
Mohammad Al-Fetyani <m4bh@hotmail.com>
2021-01-28 15:25:09 +0200

    fix test

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -2,10 +2,11 @@ from xinaprocessor.cleaners import *
 from test_const import *
 import pytest
 
+
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_english_text), 
-    (text_test_example_2, example_2_remove_english_text), 
-    (text_test_example_3, example_3_remove_english_text), 
+    (text_test_example_1, example_1_remove_english_text),
+    (text_test_example_2, example_2_remove_english_text),
+    (text_test_example_3, example_3_remove_english_text),
     (text_test_example_4, example_4_remove_english_text)
 ])
 def test_remove_english_text(inp_text, target_text):
@@ -14,9 +15,9 @@ def test_remove_english_text(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_arabic_text), 
-    (text_test_example_2, example_2_remove_arabic_text), 
-    (text_test_example_3, example_3_remove_arabic_text), 
+    (text_test_example_1, example_1_remove_arabic_text),
+    (text_test_example_2, example_2_remove_arabic_text),
+    (text_test_example_3, example_3_remove_arabic_text),
     (text_test_example_4, example_4_remove_arabic_text)
 ])
 def test_remove_arabic_text(inp_text, target_text):
@@ -25,9 +26,9 @@ def test_remove_arabic_text(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_arabic_text), 
-    (text_test_example_2, example_2_remove_arabic_text), 
-    (text_test_example_3, example_3_remove_arabic_text), 
+    (text_test_example_1, example_1_remove_arabic_text),
+    (text_test_example_2, example_2_remove_arabic_text),
+    (text_test_example_3, example_3_remove_arabic_text),
     (text_test_example_4, example_4_remove_arabic_text)
 ])
 def test_remove_numbers(inp_text, target_text):
@@ -36,9 +37,9 @@ def test_remove_numbers(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_numbers), 
-    (text_test_example_2, example_2_remove_numbers), 
-    (text_test_example_3, example_3_remove_numbers), 
+    (text_test_example_1, example_1_remove_numbers),
+    (text_test_example_2, example_2_remove_numbers),
+    (text_test_example_3, example_3_remove_numbers),
     (text_test_example_4, example_4_remove_numbers)
 ])
 def test_remove_numbers(inp_text, target_text):
@@ -47,9 +48,9 @@ def test_remove_numbers(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_arabic_numbers), 
-    (text_test_example_2, example_2_remove_arabic_numbers), 
-    (text_test_example_3, example_3_remove_arabic_numbers), 
+    (text_test_example_1, example_1_remove_arabic_numbers),
+    (text_test_example_2, example_2_remove_arabic_numbers),
+    (text_test_example_3, example_3_remove_arabic_numbers),
     (text_test_example_4, example_4_remove_arabic_numbers)
 ])
 def test_remove_arabic_numbers(inp_text, target_text):
@@ -58,9 +59,9 @@ def test_remove_arabic_numbers(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_english_numbers), 
-    (text_test_example_2, example_2_remove_english_numbers), 
-    (text_test_example_3, example_3_remove_english_numbers), 
+    (text_test_example_1, example_1_remove_english_numbers),
+    (text_test_example_2, example_2_remove_english_numbers),
+    (text_test_example_3, example_3_remove_english_numbers),
     (text_test_example_4, example_4_remove_english_numbers)
 ])
 def test_remove_english_numbers(inp_text, target_text):
@@ -69,30 +70,31 @@ def test_remove_english_numbers(inp_text, target_text):
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_arabic_punctuation), 
-    (text_test_example_2, example_2_remove_arabic_punctuation), 
-    (text_test_example_3, example_3_remove_arabic_punctuation), 
+    (text_test_example_1, example_1_remove_arabic_punctuation),
+    (text_test_example_2, example_2_remove_arabic_punctuation),
+    (text_test_example_3, example_3_remove_arabic_punctuation),
     (text_test_example_4, example_4_remove_arabic_punctuation)
 ])
-def test_remove_arabic_punctuation(inp_text, target_text):
+def test_remove_arabic_punctuations(inp_text, target_text):
     cleaner = TextCleaner(inp_text)
-    assert cleaner.remove_arabic_punctuation().text.strip() == target_text.strip()
+    assert cleaner.remove_arabic_punctuations().text.strip() == target_text.strip()
+
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_english_punctuation), 
-    (text_test_example_2, example_2_remove_english_punctuation), 
-    (text_test_example_3, example_3_remove_english_punctuation), 
+    (text_test_example_1, example_1_remove_english_punctuation),
+    (text_test_example_2, example_2_remove_english_punctuation),
+    (text_test_example_3, example_3_remove_english_punctuation),
     (text_test_example_4, example_4_remove_english_punctuation)
 ])
-def test_remove_english_punctuation(inp_text, target_text):
+def test_remove_english_punctuations(inp_text, target_text):
     cleaner = TextCleaner(inp_text)
-    assert cleaner.remove_english_punctuation().text.strip() == target_text.strip()
+    assert cleaner.remove_english_punctuations().text.strip() == target_text.strip()
 
 
 @pytest.mark.parametrize("inp_text, target_text", [
-    (text_test_example_1, example_1_remove_punctuation), 
-    (text_test_example_2, example_2_remove_punctuation), 
-    (text_test_example_3, example_3_remove_punctuation), 
+    (text_test_example_1, example_1_remove_punctuation),
+    (text_test_example_2, example_2_remove_punctuation),
+    (text_test_example_3, example_3_remove_punctuation),
     (text_test_example_4, example_4_remove_punctuation)
 ])
 def test_remove_punctuation(inp_text, target_text):
